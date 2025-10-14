e10a0e5d30fe85012377e7b24c73cc998b5a5de2
Lluje <1000youngmin@naver.com>
2025-10-14 16:32:03 +0900

    Update BattleManager.cs

--- BattleManager.cs
+++ BattleManager.cs
@@ -3,38 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TeamTextRPG
 {
     internal class BattleManager
     {
-        string name = "임시";
+        Monster monster;
+        Character character;
         public void BattleDisPlayer()
         {
+            Console.Clear();
             Console.WriteLine("Battle!!");
             Console.WriteLine();
-            Console.WriteLine($"Lv.{1} {name} Hp. {1}");
+            Console.WriteLine($"Lv.{monster.Attack} {monster.Name} Hp. {monster.Hp}");
+            Console.WriteLine();
+            Console.WriteLine();
             Console.WriteLine("\n[내정보]");
-            Console.WriteLine($"Lv.{1} {name}({name})");
-            Console.WriteLine($"HP {1}/{1}");
+            Console.WriteLine($"Lv.{character.Level} {character.Name}({character.Job})");
+            Console.WriteLine($"HP {character.Hp}/{character.Hp}");
             Console.WriteLine();
             Console.WriteLine("1. 공격");
             Console.WriteLine("2. 도주");
             Console.WriteLine();
             Console.WriteLine("원하는 행동을 입력");
             Console.Write(">>");
-            Console.ReadLine();
+            int act1;
+            if (int.TryParse(Console.ReadLine(), out act1))
+            {
+                switch (act1)
+                {
+                    case 1:
+                        Console.Clear();
+                        AttackDisplayer();
+                        break;
+                }
+            }
+            else
+            {
+                Console.Clear();
+                BattleDisPlayer();
+            }
         }
 
-        public void Attack()
+        public void AttackDisplayer()
         {
+            Console.Clear();
             Console.WriteLine("Battle!!");
             Console.WriteLine();
-            Console.WriteLine($"1 Lv.{1} {name} Hp. {1}");
+            Console.WriteLine($"1  Lv.{monster.Attack} {monster.Name} Hp. {monster.Hp}");
             Console.WriteLine("\n[내정보]");
-            Console.WriteLine($"Lv.{1} {name}({name})");
-            Console.WriteLine($"HP {1}/{1}");
+            Console.WriteLine($"Lv.{character.Level} {character.Name}({character.Job})");
+            Console.WriteLine($"HP {character.Hp}/{character.Hp}");
             Console.WriteLine();
             Console.WriteLine("0. 취소");
             Console.WriteLine();
