513fd7044abd3c520c14c2430a8fb54513cc8224
jaewoo0530 <6672jjw@gmail.com>
2025-10-14 16:02:46 +0900

    Add: 몬스터 종류 생성

--- Monster.cs
+++ Monster.cs
@@ -12,10 +12,14 @@ namespace TeamTextRPG
 
         private Random random = new Random();
 
-        public Monster(string name, int basePower, int baseDefense, int hp): base(name, basePower, baseDefense, hp)
+        public Monster(string name, int attack, int defense, int hp): base(name, attack, defense, hp)
         {
             Monsters = new List<Monster>
             {
+                new Monster("저글링", 3, 0, 20),
+                new Monster("히드라", 7, 3, 40),
+                new Monster("뮤탈", 10, 6, 100),
+                new Monster("울라리", 25, 15, 200)
             };
         }
 
