13b131147e615cd9130b50a1527671a4e617c0f1
jaewoo0530 <6672jjw@gmail.com>
2025-10-14 17:55:28 +0900

    Add: 생성자 수정

--- MonsterData.cs
+++ MonsterData.cs
@@ -16,10 +16,10 @@ namespace TeamTextRPG
         {
             Monsters = new List<Monster>
             {
-                new Monster("저글링", 3, 0, 20),
-                new Monster("히드라", 7, 3, 40),
-                new Monster("뮤탈", 10, 6, 100),
-                new Monster("울라리", 25, 15, 200)
+                new Monster("저글링", 3, 0, 20, 1),
+                new Monster("히드라", 7, 3, 40, 3),
+                new Monster("뮤탈", 10, 6, 100, 5),
+                new Monster("울라리", 25, 15, 200, 10)
             };
         }
 
