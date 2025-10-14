abf0d84811d08682d9c841725fec6ac9cba16750
Unity12 sungjae <sj070600@gmail.com>
2025-10-14 15:02:45 +0900

    Add: Living 클래스 기본틀 생성

---
+++ Living.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamTextRPG
+{
+    internal class Living
+    {
+        public string Name { get; protected set; }
+        public int Attack { get; protected set; }
+        public int Defense { get; protected set; }
+        public int Hp { get; protected set; }
+
+        public Living(string name, int attack, int defense, int hp)
+        {
+            Name = name;
+            Attack = attack;
+            Defense = defense;
+            Hp = hp;
+        }
+
+        public virtual int CalculateDamage()
+        {
+            return Attack;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            int finaldamage = Math.Max(damage-Defense, 0);
+            Hp -= finaldamage;
+            if (Hp<0 ) Hp = 0;
+        }
+    }
+}
