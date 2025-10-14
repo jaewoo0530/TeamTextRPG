137a24d218e7fa5f79207ceb8a5b601313cb8bcf
jaewoo0530 <6672jjw@gmail.com>
2025-10-14 14:29:35 +0900

    Add: 생성자 및 베이스 추가

--- Character.cs
+++ Character.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace TeamTextRPG
 {
-    internal class Character
+    internal class Character : Living
     {
+        public string Job { get; }
+        public int Level { get; private set; } = 1;
+
+        private int stamina = 100;
+        public int Stamina
+        {
+            get { return stamina; }
+            private set
+            {
+                if (value < 0)
+                    stamina = 0;
+                else if (value > 100)
+                    stamina = 100;
+                else
+                    stamina = value;
+            }
+        }
+
+        public int Gold { get; private set; } = 500;
+        public int Exp { get; private set; } = 0;
+
+        public Character(string name, string job, int basePower, int baseDefense) : base(name, job)
+        {
+
+        }
     }
 }
