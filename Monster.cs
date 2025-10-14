05084b1d46c85bb11545803cf895335f201ef111
jaewoo0530 <6672jjw@gmail.com>
2025-10-14 15:59:22 +0900

    Add: 랜덤 몬스터 생성

--- Monster.cs
+++ Monster.cs
@@ -8,8 +8,21 @@ namespace TeamTextRPG
 {
     internal class Monster: Living
     {
+        public List<Monster> Monsters { get; private set; }
+
+        private Random random = new Random();
+
         public Monster(string name, int basePower, int baseDefense, int hp): base(name, basePower, baseDefense, hp)
         {
+            Monsters = new List<Monster>
+            {
+            };
+        }
+
+        public Monster GetRandomMonster()
+        {
+            int index = random.Next(0, Monsters.Count);
+            return Monsters[index];
         }
     }
 }
