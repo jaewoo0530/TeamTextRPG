ef8bf3b7ce92659f275d1f2c56a0b3d49e94b9f5
Cheol <ausqkf301@naver.com>
2025-10-14 14:44:01 +0900

    Add : 게임 시작 화면 UI 추가

--- UImanager.cs
+++ UImanager.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace TeamTextRPG
 {
-    internal class Class1
+    internal class UIManager
     {
+        public UIManager() // 생성자
+        {
+
+        }
+        
+        public void Mainmenu() // 게임 시작 화면
+        {
+            Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.\n이제 전투를 시작할 수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("1. 상태 보기\n2. 전투 시작");
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요.\n >>");
+        }
     }
 }
