6414ccfe9ec027117c1e89bf2069f88013098d92
Unity12 sungjae <sj070600@gmail.com>
2025-10-14 14:35:07 +0900

    Revert "Add: 몬스터 클래스 추가 및 생성자"
    This reverts commit b9b06e1ed139acf42198ea44d1f276d0b9a93da9.

--- Character.cs
+++ Character.cs
@@ -6,32 +6,7 @@ using System.Threading.Tasks;
 
 namespace TeamTextRPG
 {
-    internal class Character : Living
+    internal class Character
     {
-        public string Job { get; }
-        public int Level { get; private set; } = 1;
-
-        private int stamina = 100;
-        public int Stamina
-        {
-            get { return stamina; }
-            private set
-            {
-                if (value < 0)
-                    stamina = 0;
-                else if (value > 100)
-                    stamina = 100;
-                else
-                    stamina = value;
-            }
-        }
-
-        public int Gold { get; private set; } = 500;
-        public int Exp { get; private set; } = 0;
-
-        public Character(string name, string job, int basePower, int baseDefense) : base(name)
-        {
-
-        }
     }
 }
