c3e6f8abe2faa1d299e919a09d82eda9c9618043
Cheol <ausqkf301@naver.com>
2025-10-14 20:00:48 +0900

    fix : Input 메서드 행동 입력 출력문 추가

--- UImanager.cs
+++ UImanager.cs
@@ -23,13 +23,13 @@ namespace TeamTextRPG
                 Console.WriteLine();
                 Console.WriteLine("1. 상태 보기\n2. 전투 시작");
                 Console.WriteLine();
-                Console.WriteLine("원하시는 행동을 입력해주세요.\n >>");
                 Input();
             }
         }
 
         public int Input()
         {
+            Console.WriteLine("원하시는 행동을 입력해주세요.\n >>");
             return int.TryParse(Console.ReadLine(), out int choice) ? choice : -1;
         }
 
