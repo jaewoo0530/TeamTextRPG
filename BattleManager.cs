0353d2ce2d68a8ac8cc2db33052b26ce6974b23f
Lluje <1000youngmin@naver.com>
2025-10-14 16:31:17 +0900

    Update BattleManager.cs

--- BattleManager.cs
+++ BattleManager.cs
@@ -9,57 +9,6 @@ namespace TeamTextRPG
 {
     internal class BattleManager
     {
-        Monster monster;
-        Character character;
 
-        string name = "임시";
-        public void BattleDisPlayer()
-        {
-            Console.Clear();
-            Console.WriteLine("Battle!!");
-            Console.WriteLine();
-            Console.WriteLine($"Lv.{monster.Attack} {monster.Name} Hp. {monster.Hp}");
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("\n[내정보]");
-            Console.WriteLine($"Lv.{character.Level} {character.Name}({character.Job})");
-            Console.WriteLine($"HP {character.Hp}/{character.Hp}");
-            Console.WriteLine();
-            Console.WriteLine("1. 공격");
-            Console.WriteLine("2. 도주");
-            Console.WriteLine();
-            Console.WriteLine("원하는 행동을 입력");
-            Console.Write(">>");
-            int act1;
-            if (int.TryParse(Console.ReadLine(), out act1))
-            {
-                switch (act1)
-                {
-                    case 1:
-                        Console.Clear();
-                        AttackDisplayer();
-                        break;
-                }
-            }
-            else { Console.Clear();
-                BattleDisPlayer(); }
-        }
-
-        public void AttackDisplayer()
-        {
-            Console.Clear();
-            Console.WriteLine("Battle!!");
-            Console.WriteLine();
-            Console.WriteLine($"1  Lv.{monster.Attack} {monster.Name} Hp. {monster.Hp}");
-            Console.WriteLine("\n[내정보]");
-            Console.WriteLine($"Lv.{character.Level} {character.Name}({character.Job})");
-            Console.WriteLine($"HP {character.Hp}/{character.Hp}");
-            Console.WriteLine();
-            Console.WriteLine("0. 취소");
-            Console.WriteLine();
-            Console.WriteLine("대상을 선택 하세요.");
-            Console.Write(">>");
-            Console.ReadLine();
-        }
     }
 }
