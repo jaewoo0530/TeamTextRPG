ed13c852662f4c658f9079a327c2adfee824dcab
Lluje <1000youngmin@naver.com>
2025-10-15 16:07:01 +0900

    Add : 스킬

--- BattleManager.cs
+++ BattleManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
 using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,12 +12,14 @@ namespace TeamTextRPG
 {
     internal class BattleManager
     {
+        private GameManager gameManager;
         Random random = new Random();
 
-        public BattleManager()
+        public BattleManager(GameManager gameManager)
         {
-
+            this.gameManager = gameManager;
         }
+        private List<Monster> Monsters => gameManager.Monsters;
         public int CalculateDamage(Living attacker)
         {
             int min = (int)(attacker.Atk * 0.9f);
@@ -35,5 +38,35 @@ namespace TeamTextRPG
             }
             else { return randDamage; }
         }
+      
+        public void Skill()
+        {
+            void FullpowerAttack(Living attacker, Living target)//마나 15를 소모하여, 전력으로 돌진해서 적 하나에게 공력력의 3배의 피해를 입힙니다. 
+            {
+                int damage = (int)(attacker.Atk * 3);
+                target.TakeDamage(damage);
+            }
+
+            void ChainAttack(Living attacker)//마나를 20소모하여 랜덤한 3명에게 공격력의 2배의 피해를 입힙니다.
+            {
+                if (Monsters == null || Monsters.Count == 0)
+                {
+                    Console.WriteLine("공격할 몬스터가 없습니다!");
+                    return;
+                }
+
+                // 랜덤으로 3회 공격
+                for (int i = 0; i < 3; i++)
+                {
+                    // 등장한 몬스터 중 하나를 무작위로 선택
+                    Monster target = Monsters[random.Next(Monsters.Count)];
+
+                    int damage = (int)(attacker.Atk * 2);
+                    target.TakeDamage(damage);
+                }
+            }
+
+        }
+
     }
 }
