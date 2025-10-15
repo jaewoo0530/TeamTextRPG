30a74d1edac8bb1c7fcdfb94f3c4f5aad4131fb3
Cheol <ausqkf301@naver.com>
2025-10-15 16:31:13 +0900

    Add : 아이템 리스트 추가

--- ItemData.cs
+++ ItemData.cs
@@ -8,5 +8,17 @@ namespace TeamTextRPG
 {
     internal class ItemData
     {
+        public List<Item> items;
+
+        public ItemData()
+        {
+            items = new List<Item>
+            {
+                new Item("가죽갑옷", ItemType.방어구, 5, "가죽으로 만들어져 움직이기 편한 갑옷입니다.", false),
+                new Item("낡은 검", ItemType.무기, 5, "쉽게 볼 수 있는 낡은 검 입니다.", false),
+                new Item("낡은 지팡이", ItemType.무기, 5, "쉽게 볼 수 있는 낡은 지팡이 입니다.", false)
+            };
+            
+        }
     }
 }
