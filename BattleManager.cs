1b284b3072dfe115fe5608d628d850737874cd83
Lluje <1000youngmin@naver.com>
2025-10-15 16:51:44 +0900

    Add : 스킬사용 mp소모

--- BattleManager.cs
+++ BattleManager.cs
@@ -38,34 +38,59 @@ namespace TeamTextRPG
             }
             else { return randDamage; }
         }
-      
+
+        public bool UseMP(int amount)
+        {
+            if (player.Mp < amount)
+            {
+                Console.WriteLine($"MP가 부족합니다! (현재 MP: {player.Mp}, 필요 MP: {amount})");
+                return false;
+            }
+
+            player.Mp -= amount;
+            Console.WriteLine($"MP {amount}를 소모했습니다. 남은 MP: {player.Mp}");
+            return true;
+        }
+
+        Character player;
         public void Skill()
         {
             void FullpowerAttack(Living attacker, Living target)//마나 15를 소모하여, 전력으로 돌진해서 적 하나에게 공력력의 3배의 피해를 입힙니다. 
             {
-                int damage = (int)(attacker.Atk * 3);
-                target.TakeDamage(damage);
-            }
-
-            void ChainAttack(Living attacker)//마나를 20소모하여 랜덤한 3명에게 공격력의 2배의 피해를 입힙니다.
-            {
-                if (Monsters == null || Monsters.Count == 0)
+                int manacount = 15;
+                if (UseMP(manacount))
+                { return; }
+                else
                 {
-                    Console.WriteLine("공격할 몬스터가 없습니다!");
-                    return;
-                }
-
-                // 랜덤으로 3회 공격
-                for (int i = 0; i < 3; i++)
-                {
-                    // 등장한 몬스터 중 하나를 무작위로 선택
-                    Monster target = Monsters[random.Next(Monsters.Count)];
-
-                    int damage = (int)(attacker.Atk * 2);
+                    int damage = (int)(attacker.Atk * 3);
                     target.TakeDamage(damage);
                 }
             }
 
+            void ChainAttack(Living attacker)//마나를 20소모하여 랜덤한 3명에게 공격력의 2배의 피해를 입힙니다.
+            {
+                int manacount = 20;
+                if (UseMP(manacount))
+                { return; }
+                else
+                {
+                    if (Monsters == null || Monsters.Count == 0)
+                    {
+                        Console.WriteLine("공격할 몬스터가 없습니다!");
+                        return;
+                    }
+
+                    // 랜덤으로 3회 공격
+                    for (int i = 0; i < 3; i++)
+                    {
+                        // 등장한 몬스터 중 하나를 무작위로 선택
+                        Monster target = Monsters[random.Next(Monsters.Count)];
+
+                        int damage = (int)(attacker.Atk * 2);
+                        target.TakeDamage(damage);
+                    }
+                }
+            }
         }
 
     }
