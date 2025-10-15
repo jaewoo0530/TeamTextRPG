71b3d75f1f0809c94740c1c18e34ab071168aa11
jaewoo0530 <6672jjw@gmail.com>
2025-10-15 10:20:04 +0900

    Add: GameManager 베이스

--- GameManager.cs
+++ GameManager.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TeamTextRPG
 {
     internal class GameManager
     {
+        public Character Player { get; private set; }
+
+        public UIManager UI { get; private set; }
+        public BattleManager Battle { get; private set; }
+
+        private MonsterData monsterData;
+
+        public void StartGame()
+        {
+            string name = Console.ReadLine();
+            JobType job = Enum.TryParse(Console.ReadLine(), out JobType selectJob) ? selectJob : JobType.전사;
+
+            Player = new Character(name, job);
+
+            UI = new UIManager(this);
+
+            UI.Mainmenu();
+        }
+
+        public void StartBattle()
+        {
+
+        }
     }
 }
