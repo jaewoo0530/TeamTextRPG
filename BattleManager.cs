b2121a9c348addf3b9bd71d24ecb0ceaeca71dba
Lluje <1000youngmin@naver.com>
2025-10-15 19:11:35 +0900

    fix : 마나 소모 수정

--- BattleManager.cs
+++ BattleManager.cs
@@ -39,39 +39,35 @@ namespace TeamTextRPG
             else { return randDamage; }
         }
 
-        public bool UseMP(int amount)
-        {
-            if (player.Mp < amount)
-            {
-                Console.WriteLine($"MP가 부족합니다! (현재 MP: {player.Mp}, 필요 MP: {amount})");
-                return false;
-            }
-
-            player.Mp -= amount;
-            Console.WriteLine($"MP {amount}를 소모했습니다. 남은 MP: {player.Mp}");
-            return true;
-        }
-
         Character player;
+
         public void Skill()
         {
             void FullpowerAttack(Living attacker, Living target)//마나 15를 소모하여, 전력으로 돌진해서 적 하나에게 공력력의 3배의 피해를 입힙니다. 
             {
                 int manacount = 15;
-                if (UseMP(manacount))
-                { return; }
+                if (player.Mp < 15)
+                {
+                    Console.WriteLine($"MP가 부족합니다! (현재 MP: {player.Mp}, 필요 MP: {manacount})"); 
+                    return;
+                }
                 else
                 {
                     int damage = (int)(attacker.Atk * 3);
                     target.TakeDamage(damage);
+                    player.Mp -= manacount;
+                    Console.WriteLine($"MP {manacount}를 소모했습니다. 남은 MP: {player.Mp}");
                 }
             }
 
             void ChainAttack(Living attacker)//마나를 20소모하여 랜덤한 3명에게 공격력의 2배의 피해를 입힙니다.
             {
                 int manacount = 20;
-                if (UseMP(manacount))
-                { return; }
+                if (player.Mp < 20)
+                {
+                    Console.WriteLine($"MP가 부족합니다! (현재 MP: {player.Mp}, 필요 MP: {manacount})");
+                    return;
+                }
                 else
                 {
                     if (Monsters == null || Monsters.Count == 0)
@@ -79,7 +75,8 @@ namespace TeamTextRPG
                         Console.WriteLine("공격할 몬스터가 없습니다!");
                         return;
                     }
-
+                    player.Mp -= manacount;
+                    Console.WriteLine($"MP {manacount}를 소모했습니다. 남은 MP: {player.Mp}");
                     // 랜덤으로 3회 공격
                     for (int i = 0; i < 3; i++)
                     {
@@ -92,6 +89,5 @@ namespace TeamTextRPG
                 }
             }
         }
-
     }
 }
