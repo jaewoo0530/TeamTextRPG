3f966bc5ae26a293e6e8469790d05d855a72f67a
Cheol <ausqkf301@naver.com>
2025-10-15 15:55:41 +0900

    Add : Item 데이터 변수, Item 생성자 추가

--- Item.cs
+++ Item.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace TeamTextRPG
 {
+    public enum ItemType { 방어구, 무기 }
     internal class Item
     {
+        public ItemType ItemType { get; set; }
+
+        public string name;
+
+        public int value;
+        public string info;
+        public bool isEquip;
+
+        public Item(string name, ItemType itemType, int value, string info, bool isEquip)
+        {
+            this.name = name;
+            ItemType = itemType;
+            this.value = value;
+            this.info = info;
+            this.isEquip = false;
+        }
     }
 }
