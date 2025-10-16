dc541463ad28253bd85e8d4df7ca5b0f524091fd
Cheol <ausqkf301@naver.com>
2025-10-16 12:34:58 +0900

    Add : 장착관리 추가

--- Inventory.cs
+++ Inventory.cs
@@ -8,5 +8,31 @@ namespace TeamTextRPG
 {
     internal class Inventory
     {
+        public void InventoryEquip(List<Item> items)
+        {
+            while (true)
+            {
+                string Choice = Console.ReadLine();
+                if (int.TryParse(Choice, out int num))
+                    continue;
+                if (num == 0)
+                {
+                    //메인메뉴
+                }
+                if (num < 1 || num > items.Count)
+                {
+                    continue;
+                }
+                Item selected = items[num - 1];
+                for (int i = 0; i < items.Count; i++)
+                {
+                    if (items[i].isEquip == true && items[i].ItemType == selected.ItemType)
+                    {
+                        items[i].isEquip = false;
+                    }
+                }
+                selected.isEquip = !selected.isEquip;
+            }
+        }   
     }
 }
