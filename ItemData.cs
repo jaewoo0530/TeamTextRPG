52dba0dec0d700595dcf7a842c003f2b8336492b
Cheol <ausqkf301@naver.com>
2025-10-16 11:26:14 +0900

    Add : 아이템 종류 추가

--- ItemData.cs
+++ ItemData.cs
@@ -14,9 +14,16 @@ namespace TeamTextRPG
         {
             items = new List<Item>
             {
-                new Item("가죽갑옷", ItemType.방어구, 5, "가죽으로 만들어져 움직이기 편한 갑옷입니다.", false),
-                new Item("낡은 검", ItemType.무기, 5, "쉽게 볼 수 있는 낡은 검 입니다.", false),
-                new Item("낡은 지팡이", ItemType.무기, 5, "쉽게 볼 수 있는 낡은 지팡이 입니다.", false)
+                new Item("가죽갑옷", ItemType.방어구, 5, "가죽으로 만들어져 움직이기 편한 갑옷입니다.", false, true),
+                new Item("강철갑옷", ItemType.방어구, 8, "가죽으로 만들어져 움직이기 편한 갑옷입니다.", false, false),
+
+                new Item("낡은 검", ItemType.무기, 4, "쉽게 볼 수 있는 낡은 검 입니다.", false, true),
+                new Item("낡은 지팡이", ItemType.무기, 8, "쉽게 볼 수 있는 낡은 지팡이 입니다.", false, true),
+                new Item("낡은 활", ItemType.무기, 2, "쉽게 볼 수 있는 낡은 지팡이 입니다.", false, true),
+
+                new Item("강철 검", ItemType.무기, 8, "쉽게 볼 수 있는 낡은 검 입니다.", false, false),
+                new Item("고목 지팡이", ItemType.무기, 12, "쉽게 볼 수 있는 낡은 지팡이 입니다.", false, false),
+                new Item("은의 활", ItemType.무기, 4, "쉽게 볼 수 있는 낡은 지팡이 입니다.", false, false)
             };
             
         }
