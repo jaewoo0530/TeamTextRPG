1efc5f0b0435a110c13f1e00eea366d64ea4586a
Cheol <ausqkf301@naver.com>
2025-10-16 14:29:05 +0900

    Fix : UI, GameManager에서 Inventory를 받아서 사용할 수 있도록 코드를 수정

--- Inventory.cs
+++ Inventory.cs
@@ -8,31 +8,21 @@ namespace TeamTextRPG
 {
     internal class Inventory
     {
-        public void InventoryEquip(List<Item> items)
+        public void InventoryEquip(List<Item> items, int choice)
         {
-            while (true)
-            {
-                string Choice = Console.ReadLine();
-                if (int.TryParse(Choice, out int num))
-                    continue;
-                if (num == 0)
-                {
-                    //메인메뉴
-                }
-                if (num < 1 || num > items.Count)
-                {
-                    continue;
-                }
-                Item selected = items[num - 1];
+                if (choice < 1 || choice > items.Count)
+
+                    return;
+
+                Item selected = items[choice - 1];
                 for (int i = 0; i < items.Count; i++)
                 {
-                    if (items[i].isEquip == true && items[i].ItemType == selected.ItemType)
+                    if (items[i].isEquip && items[i].ItemType == selected.ItemType)
                     {
                         items[i].isEquip = false;
                     }
                 }
                 selected.isEquip = !selected.isEquip;
-            }
         }   
     }
 }
