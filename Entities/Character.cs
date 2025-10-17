bcc249d0a3bcae21b4a0aca7a71af2afc0c51282
jaewoo0530 <6672jjw@gmail.com>
2025-10-17 11:29:02 +0900

    Add: 기본 스탯, 아이템 스탯 구분 및 계산

--- Entities/Character.cs
+++ Entities/Character.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TeamTextRPG.Items;
 
 namespace TeamTextRPG.Entities
 {
@@ -11,6 +12,10 @@ namespace TeamTextRPG.Entities
     {
         public JobType Job { get; }
 
+        public int BaseAtk { get; private set; }
+        public int BaseDef { get; private set; }
+        public int ItemAtk { get; private set; }
+        public int ItemDef { get; private set; }
         public int Gold { get; private set; } = 500;
         public int Exp { get; private set; } = 0;
 
@@ -29,6 +34,9 @@ namespace TeamTextRPG.Entities
             }
         }
 
+        private Item equippedAttackItem;
+        private Item equippedDefenseItem;
+
         public Character(string name, JobType job)
         {
             Name = name;
@@ -37,19 +45,54 @@ namespace TeamTextRPG.Entities
 
             switch (job)
             {
-                case JobType.전사: Atk = 5; Def = 10; MaxHp = 100; break;
-                case JobType.궁수: Atk = 10; Def = 5; MaxHp = 100; break;
-                case JobType.마법사: Atk = 3; Def = 5; MaxHp = 100; break;
+                case JobType.전사: BaseAtk = 5; BaseDef = 10; MaxHp = 100; break;
+                case JobType.궁수: BaseAtk = 10; BaseDef = 5; MaxHp = 100; break;
+                case JobType.마법사: BaseAtk = 3; BaseDef = 5; MaxHp = 100; break;
             }
 
             Hp = MaxHp;
             Mp = 100;
+
+            CalculateItemStat();
         }
 
         public void AddExp(int value)
         {
             Exp += value;
             LevelUp();
+        }
+
+        public void ApplyItem(Item item)
+        {
+            switch (item.ItemType)
+            {
+                case ItemType.무기:
+                    equippedAttackItem = item.isEquip ? item : null;
+                    break;
+                case ItemType.방어구:
+                    equippedDefenseItem = item.isEquip ? item : null;
+                    break;
+                case ItemType.소모템:
+                    Hp += item.value;
+                    break;
+            }
+
+            CalculateItemStat();
+        }
+         
+        public void CalculateItemStat()
+        {
+            ItemAtk = 0;
+            ItemDef = 0;
+
+            if (equippedAttackItem != null)
+                ItemAtk += equippedAttackItem.value;
+
+            if (equippedDefenseItem != null)
+                ItemDef += equippedDefenseItem.value;
+
+            Atk = BaseAtk + ItemAtk;
+            Def = BaseDef+ ItemDef;
         }
 
         public void LevelUp()
