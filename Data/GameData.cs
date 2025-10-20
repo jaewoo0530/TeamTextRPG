1f5d82c2227de6b777d49f14e4c204f5f8aa3f8e
Unity12 sungjae <sj070600@gmail.com>
2025-10-20 12:17:07 +0900

    Add: GameData, Fix: SaveManager

---
+++ Data/GameData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamTextRPG.Entities;
+using TeamTextRPG.Items;
+using TeamTextRPG.Manager;
+
+namespace TeamTextRPG.Data
+{
+    internal class GameData
+    {
+        public Character Player { get; set; }
+        public Inventory Inventory { get; set; }
+        public QuestManager QuestManager { get; set; }
+        public int StageNumber { get; set; }
+    }
+}
