e4e70597d805900bbac4c13f3a2a2a0408da054d
jaewoo0530 <6672jjw@gmail.com>
2025-10-20 12:39:43 +0900

    Fix: 아이템 값 수정

--- Items/Inventory.cs
+++ Items/Inventory.cs
@@ -17,8 +17,8 @@ namespace TeamTextRPG.Items
             {
                 new Item("가죽갑옷", ItemType.방어구, 5, "가죽으로 만들어져 움직이기 편한 갑옷입니다.", false, true),
                 new Item("낡은 검", ItemType.무기, 4, "쉽게 볼 수 있는 낡은 검입니다.", false, true),
-                new Item("낡은 지팡이", ItemType.무기, 8, "쉽게 볼 수 있는 낡은 지팡이입니다.", false, true),
-                new Item("낡은 활", ItemType.무기, 2, "쉽게 볼 수 있는 낡은 활입니다.", false, true),
+                new Item("낡은 지팡이", ItemType.무기, 2, "쉽게 볼 수 있는 낡은 지팡이입니다.", false, true),
+                new Item("낡은 활", ItemType.무기, 3, "쉽게 볼 수 있는 낡은 활입니다.", false, true),
             };
 
             consumableItems = new List<Item>()
